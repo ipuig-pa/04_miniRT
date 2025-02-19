a00175d3be841b24655697dcb40c017324e87d58
Irene Puig Parnau <ipuig-pa@1-C-24.42heilbronn.de>
2025-02-19 10:41:46 +0100

    more org

---
+++ src/rendering/render.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 10:40:28 by ipuig-pa          #+#    #+#             */
+/*   Updated: 2025/02/19 10:41:24 by ipuig-pa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "linalg.h"
+#include "scene.h"
+
+//anything related with the tracing of the rays and render of the image
